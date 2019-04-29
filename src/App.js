dbffd413e4e53bc0c8173774a878407c1f5c40b5
SSSofyen <selmane.sofyen@gmail.com>
2019-04-29 18:25:37 +0200

    part 2

--- src/App.js
+++ src/App.js
@@ -23,7 +23,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      popoverOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      popoverOpen: !this.state.popoverOpen
+    });
+  }
+
   render() {
+
+    var moviesNameList = [
+      "a",
+      "b"
+    ]
+
+    var moviesLast ="aucun film sélectionné"
+
+    if (moviesNameList.length > 0) {
+      moviesLast = moviesNameList.pop();
+    }
+    if (moviesNameList.length > 0) {
+      moviesLast =  moviesLast + ', ' + moviesNameList.pop();
+    }
+    if (moviesNameList.length > 0) {
+      moviesLast =  moviesLast + ', ' + moviesNameList.pop();
+    }
+    if (moviesNameList.length > 0) {
+      moviesLast = moviesLast + ' ...'
+    }
+
+console.log(moviesLast)
+    var moviesCount = moviesNameList.length
+    console.log(moviesCount)
+
 
     var movieData = [
       {
@@ -43,9 +83,9 @@ class App extends Component {
       }
     ]
 
-    var movielist = movieData.map(
-      function(){
-        return(<MovieCard/>)
+    var movieList = movieData.map(
+      function(movie){
+        return(<Movie movieName={movie.name} moviedesc={movie.desc} movieImg={movie.img}/>)
       }
     )
 
@@ -77,11 +117,18 @@ class App extends Component {
                 color: '#FFFFFF'
               }}>MyMoviz</NavLink>
           </NavItem>
+
+          <Button id="Popover1" onClick={this.toggle}>
+          </Button>
+          <Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+            <PopoverBody>{moviesLast}</PopoverBody>
+          </Popover>
+
       </Nav>
       </Col>
 
       <Row xs="12" md="6" lg="4">
-      {movielist}
+      {movieList}
     </Row>
 
   </Row>
@@ -91,19 +138,20 @@ class App extends Component {
   }
 }
 
-const MovieCard =()=> {
+class Movie extends Component {
+render () {
   return (
     <Col>
       <Card style={{marginBottom:15}}>
         <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
         <CardBody>
           <FontAwesomeIcon style={{position:"left", left:"90%"}} icon={faHeart}/>
-          <CardTitle>movie title</CardTitle>
-          <CardText>movie description</CardText>
+          <CardTitle>{this.props.movieName}</CardTitle>
+          <CardText>{this.props.movieDesc}</CardText>
         </CardBody>
       </Card>
     </Col>
   );
 };
-
+}
 export default App;
